26db3a2c29ec910ee38dbd3ef7069a4b43aab8f1
Melat Belachew <melbelachew@gmail.com>
2018-12-06 23:58:47 -0500

    i

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,22 @@
-var name = promt("What is your name?"); 
-            alert("Hello"+" "+"name");
+//the user has to guess which letter the app chose. 
+
+var userName = prompt("What is your name?")
+
+alert("Hello" +" "+ userName);
+
 var wannaplay= confirm("Do you want to play a guessing game?") 
-                if (wannaplay=true){alert("guess what letter I am thinking?")}
-                else {alert("goodbye" + " " + "name")}
+
+if (wannaplay=true){
+
+alert("Guess any letter from A through L?")}
+
+else {alert("Goodbye" + " " + userName)}
 
             
 
 
 
-// You're going to make a game just like the one in the video. Essentially, the app randomly picks a letter, and the user has to guess which letter the app chose. Put the following text on your page:
-// Guess what letter I'm thinking of
+// randomly picks a letter
 // Wins: (# of times the user has guessed the letter correctly)
 // Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
 // Guesses Left: (# of guesses left. This will update)
@@ -18,37 +25,31 @@ var wannaplay= confirm("Do you want to play a guessing game?")
 // When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
 // //declair variables for logging wins, losses, users guesses & guess left
 
-// var wins = 0;
-// var losses = 0;
-// var numberOfGuesses = 9;
-// var yourGuessLeft = [];
+var wins = 0;
+var losses = 0;
+var gussesLeft =9;
+var yourGuesses=[];
 
-// //declare a function to listen to events on releas of any key 
-// document.onkeyup = function(e) {
-//     var userGuess = event.key;
+var randSelection = ["a","b","c","d","e","f","g","h","i","k","l"]
 
-//     var computerGues = 
+var computerGuess = randSelection[Math.floor(Math.random()*randSelection.length)];
+
+document.onkeyup=function(event){
+var userGuess = event.key;
+if (userGuess==computerGuess) {
+    wins++; yourGuesses.push(userGuess)
+}
+else if(userGuess!=computerGuess) {
+    gussesLeft--; losses++; yourGuesses.push(userGuess)
+}}
+
+document.querySelector("#Guess").html = 
+"<p>Wins: " + wins + "</p>" +
+"<p>Losses: " + losses + "</p>" +
+"<p>Guesses Left: " + gussesLeft + "</p>" +
+"<p>Your Guesses so far: " + yourGuesses.join + "</p>"
 
 
 
 
-// //create secretNumber
-// var secretNumber = 4;
 
-// //ask user for guess
-// var stringGuess = prompt("Guess a number");
-// var guess = Number(stringGuess);
-
-// //check if guess is right
-// if(guess === secretNumber) {
-// 	alert("YOU GOT IT RIGHT!");
-// }
-// //check if guess is higher
-// else if(guess > secretNumber){
-// 	alert("Too high.  Guess again!");
-// }
-
-// else {
-// 	alert("Too low.  Guess again!");
-// }
-// //otherwise, check if lower
